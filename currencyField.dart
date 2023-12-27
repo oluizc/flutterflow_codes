f94bf9f3a4e8e3706b53da365efad479c3bb273a
oluizc <150074900+oluizc@users.noreply.github.com>
2023-12-27 10:41:25 -0300

    Update currencyField.dart

--- currencyField.dart
+++ currencyField.dart
@@ -14,6 +14,7 @@ class CurrencyTextField extends StatefulWidget {
     required this.borderRadius,
     required this.borderColor,
     required this.borderColorFocus,
+    required this.onChangeAction,
   }) : super(key: key);
 
   final double? width;
@@ -24,6 +25,7 @@ class CurrencyTextField extends StatefulWidget {
   final double borderRadius;
   final Color borderColor;
   final Color borderColorFocus;
+  final Future<dynamic> Function() onChangeAction;
 
   @override
   _CurrencyTextFieldState createState() => _CurrencyTextFieldState();
@@ -33,44 +35,49 @@ class _CurrencyTextFieldState extends State<CurrencyTextField> {
   bool isHovered = false;
 
   @override
+  void initState() {
+    super.initState();
+
+    // Chamado quando o widget é inserido na árvore de widgets pela primeira vez
+    FFAppState().valorCurrencyField1 = widget.valor;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) => setState(() => isHovered = true),
-      onExit: (_) => setState(() => isHovered = false),
-      child: TextFormField(
-        initialValue: widget.valor,
-        keyboardType: TextInputType.number,
-        inputFormatters: [
-          CurrencyTextInputFormatter(
-            locale: 'pt_BR',
-            decimalDigits: 2,
-            symbol: 'R\$',
-            enableNegative: false,
-          ),
-          LengthLimitingTextInputFormatter(16),
-        ],
-        style: TextStyle(
-          fontWeight: FontWeight.normal,
-          color: widget.colorText,
-          fontSize: widget.fontSize,
+    return TextFormField(
+      initialValue: widget.valor,
+      keyboardType: TextInputType.number,
+      inputFormatters: [
+        CurrencyTextInputFormatter(
+          locale: 'pt_BR',
+          decimalDigits: 2,
+          symbol: 'R\$',
+          enableNegative: false,
         ),
-        decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(widget.borderRadius),
-            borderSide: BorderSide(color: widget.borderColor, width: 1.0),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(widget.borderRadius),
-            borderSide: BorderSide(color: widget.borderColorFocus),
-          ),
-          filled: false,
-        ),
-        onChanged: (text) {
-          print("valor fomartValue");
-          print(text);
-          FFAppState().valorCurrencyField1 = text;
-        },
+        LengthLimitingTextInputFormatter(16),
+      ],
+      style: TextStyle(
+        fontWeight: FontWeight.normal,
+        color: widget.colorText,
+        fontSize: widget.fontSize,
       ),
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(widget.borderRadius),
+          borderSide: BorderSide(color: widget.borderColor, width: 1.0),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(widget.borderRadius),
+          borderSide: BorderSide(color: widget.borderColorFocus),
+        ),
+        filled: false,
+      ),
+      onChanged: (text) {
+        print(text);
+        FFAppState().valorCurrencyField1 = text;
+        //Ação para capturar o valor do app state e colocar em outro state (page ou component)
+        widget.onChangeAction();
+      },
     );
   }
 }
